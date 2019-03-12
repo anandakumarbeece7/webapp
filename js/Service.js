c6c43a75ae87068fbacf60e5ff83e1423f3165ba
Jegadeesh <cjegadeesh@acestranetworks.com>
2019-03-12 16:00:37 +0530

    anand:upto date 05/03/2019 complete

---
+++ js/Service.js
@@ -0,0 +1,23 @@
+﻿app.factory('api_Service', function ($http) {
+    return {
+        get_list: function (url) {
+            console.log(url);
+            return $http.get(api_url+url).then(function (data) {
+                return data.data;
+            });
+        },
+        post_list: function (url, obj) {
+            console.log(url);
+            return $http.post(api_url+url, obj).then(function (data) {
+                return data.data;
+            });
+        },
+       
+        
+
+    }
+});
+
+app.config(['$qProvider', function ($qProvider) {
+    $qProvider.errorOnUnhandledRejections(false);
+}]);
---
+++ js/app.js
@@ -0,0 +1,4 @@
+﻿var app = angular.module("myApp", ['angularUtils.directives.dirPagination']);
+var api_url = 'http://localhost:49978/';
+
+
